a077e32e4511f83f6454a27a874b04ed4966d843
Ayoola FALOLA <ayoola@ayoo.la>
2021-12-11 21:52:16 +0100

    First fix

--- application/pages/includes/app.php
+++ application/pages/includes/app.php
@@ -5,43 +5,50 @@
 * LICENSE
 *
 * @category PageCarton
-* @package /sales-center
+* @package /app
 * @generated Ayoola_Page_Editor_Layout
 * @copyright  Copyright (c) PageCarton. (http://www.PageCarton.com)
 * @license    http://www.PageCarton.com/license.txt
-* @version $Id: sales-center.php	Saturday 11th of December 2021 08:10:55 PM	ayoola.falola@yahoo.com $ 
+* @version $Id: app.php	Saturday 11th of December 2021 08:49:10 PM	ayoola.falola@yahoo.com $ 
 */
 //	Page Include Content
 
 							if( Ayoola_Loader::loadClass( 'Ayoola_Page_Editor_Text' ) )
 							{
 								
-$_aca0c54c31f0763108a62e561d49a6c0 = new Ayoola_Page_Editor_Text( array (
-  '' => NULL,
-  'editable' => '
-<div class="x-content">
+$_a3160f44bdc7b6862bc39413b23cc75e = new Ayoola_Page_Editor_Text( array (
+  'codes' => '<div class="x-content">
                 <include href="/pos/layout/pc_layout_possystem/pos.html"></include>
 </div>
 
 ',
+  'preserved_content' => '<div class="x-content">
+                <include href="/pos/layout/pc_layout_possystem/pos.html"></include>
+</div>
+
+',
+  'url_prefix' => '/pos',
+  'widget_options' => 
+  array (
+    0 => 'preserve_content',
+  ),
+  'pagewidget_id' => '0-1639255747-5',
   'includes' => 
   array (
     '/layout/pc_layout_possystem/pos' => '<include href="/layout/pc_layout_possystem/pos.html"></include>',
   ),
-  'content' => '
-<div class="x-content">
+  'content' => '<div class="x-content">
                 <include href="/layout/pc_layout_possystem/pos.html"></include>
 </div>
 
 ',
-  'pagewidget_id' => NULL,
 ) );
 
 							}
 							else
 							{
 								
-$_aca0c54c31f0763108a62e561d49a6c0 = null;
+$_a3160f44bdc7b6862bc39413b23cc75e = null;
 
 							}
 							
---
+++ application/pages/includes/sales-center.php
@@ -0,0 +1,70 @@
+<?php
+/**
+* PageCarton Page Generator
+*
+* LICENSE
+*
+* @category PageCarton
+* @package /sales-center
+* @generated Ayoola_Page_Editor_Layout
+* @copyright  Copyright (c) PageCarton. (http://www.PageCarton.com)
+* @license    http://www.PageCarton.com/license.txt
+* @version $Id: sales-center.php	Saturday 11th of December 2021 08:47:34 PM	ayoola.falola@yahoo.com $ 
+*/
+//	Page Include Content
+
+							if( Ayoola_Loader::loadClass( 'Ayoola_Page_Editor_Text' ) )
+							{
+								
+$_af23a45f9466e257f8c0162b46cf5e30 = new Ayoola_Page_Editor_Text( array (
+  'codes' => '<div class="x-content">
+                <include href="/pos/layout/pc_layout_possystem/pos.html"></include>
+</div>
+
+',
+  'preserved_content' => '',
+  'url_prefix' => '/pos',
+  'pagewidget_id' => '0-1639255523-1',
+  'includes' => 
+  array (
+    '/layout/pc_layout_possystem/pos' => '<include href="/layout/pc_layout_possystem/pos.html"></include>',
+  ),
+  'content' => '<div class="x-content">
+                <include href="/layout/pc_layout_possystem/pos.html"></include>
+</div>
+
+',
+) );
+
+							}
+							else
+							{
+								
+$_af23a45f9466e257f8c0162b46cf5e30 = null;
+
+							}
+							
+							if( Ayoola_Loader::loadClass( 'Ayoola_Page_Editor_Text' ) )
+							{
+								
+$_30482803a57fbfd6f2b8ce40570791a5 = new Ayoola_Page_Editor_Text( array (
+  'editable' => '',
+  'preserved_content' => '			
+			<h3>Hello</h3>
+			',
+  'url_prefix' => '/pos',
+  'pagewidget_id' => '0-1639255594-2',
+  'includes' => 
+  array (
+  ),
+  'content' => '',
+) );
+
+							}
+							else
+							{
+								
+$_30482803a57fbfd6f2b8ce40570791a5 = null;
+
+							}
+							
